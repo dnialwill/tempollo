d0df3e3e0b9c0ce72ecea5345f25dda14ebf42cc
dnialwill <dnialwill@gmail.com>
2018-11-08 17:13:08 -0500

    Added jest support

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,19 +10,19 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  test('the app is created', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tempollo'`, () => {
+  test(`the title is 'tempollo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('tempollo');
   });
 
-  it('should render title in a h1 tag', () => {
+  test('the title is in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
